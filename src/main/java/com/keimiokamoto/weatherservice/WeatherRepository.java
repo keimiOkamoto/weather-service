31c98f5e7cd7166af5ceb6f1679797b886292363
keimiokamoto <keimiokamoto@skyscanner.net>
2022-03-01 14:35:18 +0000

    Can retrieve data from WeatherRepository

---
+++ src/main/java/com/keimiokamoto/weatherservice/WeatherRepository.java
@@ -0,0 +1,26 @@
+package com.keimiokamoto.weatherservice;
+
+import java.awt.geom.Point2D;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class WeatherRepository {
+
+  private static volatile Map<Point2D.Double, String> stationLocationToWeatherMap = new HashMap<>();
+  private static volatile Map<Point2D.Double, String> cityLocationToCityNameMap = new HashMap<>();
+
+  public WeatherRepository() {
+    stationLocationToWeatherMap = DataLoaderUtil.loadWeatherStations();
+    cityLocationToCityNameMap = DataLoaderUtil.loadCities();
+  }
+
+  public Map<Point2D.Double, String> getStationLocationToWeatherMap() {
+    return stationLocationToWeatherMap;
+  }
+
+  public Map<Point2D.Double, String> getCityLocationToCityNameMap() {
+    return cityLocationToCityNameMap;
+  }
+}
---
+++ src/test/java/com/keimiokamoto/weatherservice/WeatherRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.keimiokamoto.weatherservice;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.awt.geom.Point2D;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class WeatherRepositoryTest {
+
+  @InjectMocks
+  private static WeatherRepository weatherRepository;
+
+  @Test
+  public void shouldGetStationLocationToWeatherMap() {
+    Map<Point2D.Double, String> stationLocationToWeatherMap = weatherRepository
+        .getStationLocationToWeatherMap();
+    assertFalse(stationLocationToWeatherMap.isEmpty());
+  }
+
+  @Test
+  public void shouldGetCityLocationToCityNameMap() {
+    Map<Point2D.Double, String> cityLocationToCityNameMap = weatherRepository
+        .getCityLocationToCityNameMap();
+    assertFalse(cityLocationToCityNameMap.isEmpty());
+  }
+}
